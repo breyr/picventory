715b335a02183ee26480a5086f673971c8824eb2
Brey Rivera <breyr2021@gmail.com>
2024-05-29 13:51:53 -0400

    refactor: Update ProviderButton to use signInWithPopup instead of signInWithRedirect

--- src/lib/components/ProviderButton.svelte
+++ src/lib/components/ProviderButton.svelte
@@ -1,29 +1,22 @@
 <script lang="ts">
     import { auth } from '$lib/firebase';
-    import { GoogleAuthProvider, getRedirectResult, signInWithRedirect } from 'firebase/auth';
-    import { onMount } from 'svelte';
+    import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
     async function signInWithGoogle() {
         const provider = new GoogleAuthProvider();
-        // automatically updates user store when onAuthStateChange
-        await signInWithRedirect(auth, provider);
-    }
+        const credential = await signInWithPopup(auth, provider);
 
-    onMount(async () => {
-        const credential = await getRedirectResult(auth);
-        const idToken = await credential?.user.getIdToken();
-        // have to check because onMount will happen without a redirect at first
-        if (idToken) {
-            const res = await fetch("/api/signin", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    // 'CSRF-Token': csrfToken  // HANDLED by sveltekit automatically
-                },
-                body: JSON.stringify({ idToken }),
-            });
-        }
-    });
+        const idToken = await credential.user.getIdToken();
+
+        const res = await fetch("/api/signin", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                // 'CSRF-Token': csrfToken  // HANDLED by sveltekit automatically
+            },
+            body: JSON.stringify({ idToken }),
+        });
+    }
 </script>
 
 <button class="btn btn-success" on:click={signInWithGoogle}>
