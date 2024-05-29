9f49e2255bc68a9bc52c54f60c73287cc740896a
Brey Rivera <breyr2021@gmail.com>
2024-05-29 13:12:56 -0400

    remove proxy

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,4 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	server: {
-		proxy: {
-			'/__/auth/': {
-				target: 'https://piventory.firebaseapp.com',
-				changeOrigin: true,
-			},
-		},
-	},
 });
